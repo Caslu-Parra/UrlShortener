fd3bb9cc48707f8ea2343220eff087119f9c92c1
Caslu-Parra <lucasparra2@outlook.com>
2023-11-21 09:04:37 -0300

    231121L_1 AddAzSecretKeys

--- Program.cs
+++ Program.cs
@@ -1,13 +1,21 @@
+using Azure.Identity;
 using Microsoft.EntityFrameworkCore;
 using UrlShortener.Database;
 using UrlShortener.Routes;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<DbConnection>(options
-    => options.UseMySql(builder.Configuration.GetConnectionString("DataBase"), ServerVersion.Parse("8.0")));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+Uri azKey = new Uri(builder.Configuration.GetSection("KeyVaultURL").Value!);
+var azCredential = new DefaultAzureCredential();
+builder.Configuration.AddAzureKeyVault(azKey, azCredential);
+
+var cs = builder.Configuration.GetSection("dbcon").Value;
+
+builder.Services.AddDbContext<DbConnection>(options
+    => options.UseMySql(cs, ServerVersion.Parse("8.0")));
 
 var app = builder.Build();
 
